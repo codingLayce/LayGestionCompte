db76a75f159fe5b6707f370d0e64d4a13bc5eabe
lucas Guilbert <lucas.guilbert15@gmail.com>
2018-10-28 16:16:47 +0100

    Ajout dialog avec erreur exception. Mise en place dialogs ouverture.

--- src/fr/layce/V2_0/controleur/ControleurFX.java
+++ src/fr/layce/V2_0/controleur/ControleurFX.java
@@ -5,6 +5,7 @@ import fr.layce.V2_0.modele.Transaction;
 import fr.layce.V2_0.modele.exceptions.TransactionException;
 import fr.layce.V2_0.vue.Fenetre;
 import fr.layce.V2_0.vue.dialog.AssistantTransaction;
+import fr.layce.V2_0.vue.dialog.Dialogs;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.event.Event;
@@ -28,33 +29,38 @@ public class ControleurFX {
 
     private ControleurFX(){
         this.compte = null;
-        this.statut = new SimpleStringProperty("Initialisation");
         this.fenetre = Fenetre.getInstance();
+        this.statut = new SimpleStringProperty("Chargement OK");
         this.fenetre.setStatut(statut);
     }
 
     /**
      * Créer un nouveau fichier.
+     * Appelée au click dans le menu et la barre d'outils.
      */
     public void nouveauFichier(Event e){
+        this.statut.setValue("Création du compte...");
         this.compte = Compte.getInstance();
         this.fenetre.compteCreated();
         this.fenetre.setData(FXCollections.observableArrayList(this.compte.getTransactions()));
-        // TODO: créer un nouveau fichier.
+        this.statut.setValue("Création du compte OK");
     }
 
     /**
      * Ouvre un nouveau fichier.
+     * Appelée au click dans le menu et la barre d'outils.
      */
     public void ouvrirFichier(Event e){
+        this.statut.setValue("Ouverture d'un compte...");
         try {
             this.compte = Compte.getInstance();
-            this.compte.ouvrirCompte(new File("test.json"));
+            this.compte.ouvrirCompte(new File("caca.json"));
             this.fenetre.compteCreated();
             this.fenetre.setData(FXCollections.observableArrayList(this.compte.getTransactions()));
+            this.statut.setValue("Compte ouvert");
         } catch (IOException ex){
-            ex.printStackTrace();
-            // TODO: afficher erreur.
+            Dialogs.errorMessage("Ouverture de compte", ex);
+            this.statut.setValue("Impossible d'ouvrir le compte");
         }
     }
 
@@ -92,6 +98,7 @@ public class ControleurFX {
 
     /**
      * Ouvre l'assistant d'ajout de transaction et ajoute la transaction au compte.
+     * Appelée au click dans le menu et la barre d'outils.
      */
     public void ajouterTransaction(Event e){
         if (this.compte != null){
@@ -109,7 +116,7 @@ public class ControleurFX {
 
     /**
      * Quitte l'application.
-     * @param e
+     * Appelée au click dans le menu ou au click de la croix rouge.
      */
     public void quitter(Event e) {
         // TODO: faire les tests de sauvegarde avant de quitter.
--- src/fr/layce/V2_0/vue/dialog/Dialogs.java
+++ src/fr/layce/V2_0/vue/dialog/Dialogs.java
@@ -2,7 +2,12 @@ package fr.layce.V2_0.vue.dialog;
 
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.Optional;
 
 /**
@@ -35,6 +40,37 @@ public class Dialogs {
         a.setHeaderText(null);
         a.setContentText(getFullText(content));
         a.showAndWait();
+    }
+
+    public static void errorMessage(String title, Exception ex){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(ex.getMessage());
+
+        // Create expandable Exception.
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        ex.printStackTrace(pw);
+        String exceptionText = sw.toString();
+
+        TextArea textArea = new TextArea(exceptionText);
+        textArea.setEditable(false);
+        textArea.setWrapText(true);
+
+        textArea.setMaxWidth(Double.MAX_VALUE);
+        textArea.setMaxHeight(Double.MAX_VALUE);
+        GridPane.setVgrow(textArea, Priority.ALWAYS);
+        GridPane.setHgrow(textArea, Priority.ALWAYS);
+
+        GridPane expContent = new GridPane();
+        expContent.setMaxWidth(Double.MAX_VALUE);
+        expContent.add(textArea, 0, 0);
+
+        // Set expandable Exception into the dialog pane.
+        alert.getDialogPane().setExpandableContent(expContent);
+
+        alert.showAndWait();
     }
 
     /**
